fe7aec1b8fbb4661e3f73b85075f7e4cb85085b2
David King <davidking41998@gmail.com>
2021-10-18 22:04:43 +0200

    PullPush now more OOP, data exploration moved to Jupyter Notebook

--- Explore.py
+++ Explore.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches
 import pandas as pd
+from Personalities import (
+    mbti_order,
+    type_to_name,
+    enne_order,
+    mbti_replace,
+    enne_replace,
+    enne_exclude,
+    mbti_exclude,
+)
 import re
 import seaborn as sns
 
@@ -8,137 +18,6 @@ sns.color_palette()
 
 enne = pd.read_csv("Data\Enneagram_Full.csv", names=["user", "flair"])
 mbti = pd.read_csv("Data\mbti_Full.csv", names=["user", "flair"])
-
-mbti_order = [
-    "INTJ",
-    "INTP",
-    "ENTJ",
-    "ENTP",
-    "INFJ",
-    "INFP",
-    "ENFJ",
-    "ENFP",
-    "ISTJ",
-    "ISFJ",
-    "ESTJ",
-    "ESFJ",
-    "ISTP",
-    "ISFP",
-    "ESTP",
-    "ESFP",
-]
-enne_order = [
-    "5w4",
-    "5",
-    "5w6",
-    "6w5",
-    "6",
-    "6w7",
-    "7w6",
-    "7",
-    "7w8",
-    "8w7",
-    "8",
-    "8w9",
-    "9w8",
-    "9",
-    "9w1",
-    "1w9",
-    "1",
-    "1w2",
-    "2w1",
-    "2",
-    "2w3",
-    "3w2",
-    "3",
-    "3w4",
-    "4w3",
-    "4",
-    "4w5",
-]
-
-mbti_replace = {
-    "[INTJ]": "INTJ",
-    "[INTP]": "INTP",
-    "[ENTJ]": "ENTJ",
-    "[INFP]": "INFP",
-    "[ENFP Queen] [EIE] [4w5] 8==D": "ENFP",
-    "[INTP]": "INTP",
-    "[INTP] 9w1": "INTP",
-    "[INFP] 4w3": "INFP",
-    "[INFJ]": "INFJ",
-    "[ENTP]": "ENTP",
-    "INFP: Breathe in strength, breathe out bullshit.": "INFP",
-    "[ESTJ] 6w5": "ESTJ",
-    "[INTJ] 1w2": "INTJ",
-    "ENFJ 4w5": "ENFJ",
-    "[ESTP]": "ESTP",
-    "enfp 9- all my own stunts": "ENFP",
-    "☺INFP☺4w5": "INFP",
-    "[ENFJ]": "ENFJ",
-    "[INTP] ": "INTP",
-}
-enne_in_mbti = [
-    "[ENFP Queen] [EIE] [4w5] 8==D",
-    "[INTP] 9w1",
-    "[INFP] 4w3",
-    "[ESTJ] 6w5",
-    "[INTJ] 1w2",
-    "ENFJ 4w5",
-    "enfp 9- all my own stunts",
-    "☺INFP☺4w5",
-]
-enne_replace = {
-    "~ E N T P  5 w 4 ~": "5w4",
-    "INTP 5+4": "5w4",
-    "4wTHREE HEE sp/so": "4w3",
-    "4sx INTJ": "4",
-    "ENTP 5wX sx": "5",
-    "6sp w 5": "6w5",
-    "INTP ~9?": "9",
-    "9w? ISTP": "9",
-    "INTP 5w?": "5",
-    "9(45)w8 so/sp": "9w8",
-    "(4)51 sx/sp INTJ": "4",
-    "INTP 5": "5",
-    "ENTP - type 3": "3",
-    "6": "6",
-    "SP3": "3",
-    "Infj 8": "8",
-    "Sx 4": "4",
-}
-
-enne_exclude = [
-    "749 Not necessarily in this order",
-    "546738 hexatype",
-    "9-4-6",
-    "2,7,or 9?",
-    "4w8",
-    "INFJ Sp/So",
-    "6-9-2",
-    "idk",
-    "8 5 4 sx/so NeTi",
-    "4, 9 or 7?? im still confused|INFP",
-    "5-8-4 INTJ so/sx",
-    "5-8-4 INTJ so/sx",
-    "ENTP \\\\ type me",
-    "8w1",
-    "unsure",
-    "???",
-    "?????",
-    "5w1",
-    "5w7",
-    "ENTP, not sure what enneagram.",
-    "x31 sp/so",
-    "9/2",
-    "4 or 5 idk",
-    "No god damn clue!",
-    "Somewhere in Shame",
-    "not sure yet",
-    "INFx 4/6w? (so/sp)",
-    "5 - 2 - 1 sx/sp",
-]
-mbti_exclude = ["[INFx]"]
 
 overlap = enne.merge(mbti, on="user").drop_duplicates("user")
 overlap = overlap[
@@ -188,10 +67,10 @@ ct = pd.crosstab(overlap["flair_x"], overlap["flair_y"])
 ct = ct[mbti_order]
 ct = ct.reindex(enne_order)
 
-plt, ax = plt.subplots(
+fig, ax = plt.subplots(
     2, 2, gridspec_kw={"width_ratios": [4, 1], "height_ratios": [1, 4]}
 )
-plt.suptitle("Users with flair set on r/mbti and r/Enneagram", y=0.95, fontsize=14)
+fig.suptitle("Users with flair set on r/mbti and r/Enneagram", y=0.95, fontsize=14)
 sns.heatmap(
     ct,
     annot=ct.applymap(lambda x: x if x >= 10 else ""),
@@ -210,4 +89,134 @@ ax[0][0].set_ylabel("")
 enne_counts = overlap["flair_x"].value_counts().reindex(enne_order)
 sns.barplot(x=enne_counts, y=enne_counts.index, ax=ax[1][1])
 ax[1][1].set_xlabel("")
+plt.show()
+
+sns.set_theme("notebook")
+ax = plt.subplot(2, 1, 1)
+mbti_base = mbti.value_counts("flair").head(16)
+mbti_base = mbti_base.reindex(mbti_order)
+sns.barplot(x=mbti_base.index, y=mbti_base / sum(mbti_base), ax=ax)
+plt.title("Users with flair on r/mbti")
+plt.xlabel("")
+plt.suptitle("Users with flair set on r/mbti and r/Enneagram", y=0.95, fontsize=14)
+ax = plt.subplot(2, 1, 2)
+sns.barplot(x=mbti_counts.index, y=mbti_counts / sum(mbti_counts), ax=ax)
+plt.title("Users with flair on both subreddits")
+plt.ylabel("")
+plt.show()
+mbti_base_proc = pd.DataFrame(
+    mbti_base / sum(mbti_base), columns=["% users with flair"]
+)
+mbti_base_proc["Type"] = "Just r/mbti"
+mbti_counts_proc = pd.DataFrame(
+    (mbti_counts / sum(mbti_counts)).values,
+    index=mbti_counts.index,
+    columns=["% users with flair"],
+)
+mbti_counts_proc["Type"] = "r/mbti and r/Enneagram"
+mbti_comb = pd.concat([mbti_base_proc, mbti_counts_proc])
+ax = sns.barplot(
+    x=mbti_comb["% users with flair"],
+    y=mbti_comb.index,
+    hue=mbti_comb["Type"],
+    palette="hls",
+)
+ax.xaxis.set_major_formatter(lambda x, y: f"{x*100:.1f} %")
+plt.suptitle("Distribution of users with flair set on r/mbti", y=0.95, fontsize=14)
+plt.show()
+
+enne_base = enne.value_counts("flair").head(27)
+enne_base = enne_base.reindex(enne_order)
+enne_base_proc = pd.DataFrame(
+    (enne_base / sum(enne_base)).values,
+    index=enne_base.index,
+    columns=["% users with flair"],
+)
+enne_base_proc["Type"] = "Juse r/Enneagram"
+enne_counts_proc = pd.DataFrame(
+    (enne_counts / sum(enne_counts)).values,
+    index=enne_counts.index,
+    columns=["% users with flair"],
+)
+enne_counts_proc["Type"] = "r/mbti and r/Enneagram"
+enne_comb = pd.concat([enne_base_proc, enne_counts_proc])
+
+ax = sns.barplot(
+    y=enne_comb["% users with flair"], x=enne_comb.index, hue=enne_comb["Type"]
+)
+ax.yaxis.set_major_formatter(lambda x, y: f"{x*100:.1f} %")
+plt.suptitle("Distribution of users with flair set on r/Enneagram", y=0.95, fontsize=14)
+plt.show()
+
+ax = sns.barplot(
+    x=enne_comb["% users with flair"],
+    y=enne_comb.index,
+    hue=enne_comb["Type"],
+    palette="hls",
+)
+ax.xaxis.set_major_formatter(lambda x, y: f"{x*100:.1f} %")
+plt.suptitle("Distribution of users with flair set on r/Enneagram", y=0.95, fontsize=14)
+plt.show()
+
+# shout out to jsgounot
+def change_height(ax, new_value):
+    for patch in ax.patches:
+        if a := patch.get_alpha():
+            nv = 1
+            patch.set_linewidth(0)
+            # c = patch.get_color()
+            # patch.set_edgecolor(c)
+        else:
+            nv = new_value
+        current_height = patch.get_height()
+        diff = current_height - nv
+        patch.set_height(nv)
+        patch.set_y(patch.get_y() + diff * 0.5)
+
+
+diff = enne_base_proc["% users with flair"] - enne_counts_proc["% users with flair"]
+ax = sns.barplot(x=diff, y=enne_counts_proc.index)
+for patch in ax.patches:
+    if patch.get_width() < 0:
+        patch.set_color("#ff796c")
+    else:
+        patch.set_color("#75bbfd")
+    patch.set_alpha(0.15)
+    patch.set_x(-1)
+    patch.set_width(2)
+ax = sns.barplot(
+    x=enne_base_proc["% users with flair"], y=enne_base_proc.index, color="#75bbfd"
+)
+ax = sns.barplot(
+    x=enne_counts_proc["% users with flair"] * -1,
+    y=enne_counts_proc.index,
+    color="#ff796c",
+)
+change_height(ax, 0.5)
+diff = enne_base_proc["% users with flair"] - enne_counts_proc["% users with flair"]
+ax = sns.barplot(x=diff, y=enne_counts_proc.index, color="#53fca1")
+ax.xaxis.set_major_formatter(lambda x, y: f"{abs(x*100):.0f} %")
+for patch in ax.patches:
+    if patch.get_height() == 0.8:
+        width = patch.get_width()
+        y = patch.get_y() + 0.6
+        if width > 0:
+            x = patch.get_x() + width
+            # t.set_bbox(dict(facecolor="#75bbfd", alpha=0.5, edgecolor="#75bbfd"))
+        else:
+            x = 0
+            # t.set_bbox(dict(facecolor="#ff796c", alpha=0.5, edgecolor="#ff796c"))
+        # plt.text(.12, -.8, "Difference", font="Arial", fontsize=11)
+        t = ax.text(0.137, y, f"{(width*100):.1f} %", fontsize=11)
+plt.legend(
+    [
+        matplotlib.patches.Patch(color="#ff796c"),
+        matplotlib.patches.Patch(color="#75bbfd"),
+        matplotlib.patches.Patch(color="#53fca1"),
+    ],
+    ["Flair set on both subreddits", "Flair set on Enneagram", "Difference"],
+    loc=0,
+)
+plt.title("Distribution of users with flair set on r/Enneagram", fontsize=16)
+ax.grid(False)
 plt.show()
--- PullPush.py
+++ PullPush.py
@@ -1,65 +1,104 @@
 import csv
+import datetime as dt
 import json
-from math import ceil 
+from math import ceil
 import praw
 import requests
+import time
 from tqdm import tqdm
+
 # Warning file takes a long time to run 40+ hours
 # Edit your praw.ini file to include your own credentials
 
-def get_users(subreddit, file_name):
-    url_base = "https://api.pushshift.io/reddit/search/submission/?"
-    params = {
-        "subreddit":subreddit,
-        "size":100
-    }
-    users = dict()
-    ids = []
 
-    r = requests.get(f"https://api.pushshift.io/reddit/search/submission/?subreddit={subreddit}&metadata=true&size=0")
-    meta = json.loads(r.text)
-    for i in tqdm(range(ceil(meta["metadata"]["total_results"]/100))):
-        r = requests.get(url_base, params = params)
-        try:
-            j = json.loads(r.text)
-            for post in j["data"]:
-                if flair := post["author_flair_text"]:
-                    users[post["author"]] = flair
-                ids.append(post["id"])
-        except json.decoder.JSONDecodeError:
-            print(params)
-            print(r.text)
-        params["before"] = j["data"][-1]["created_utc"]
-        if not j["data"]:
-            break
+class UserGetter:
+    def __init__(self, url_base=None, params=None, users=dict()):
+        self.url_base = (
+            url_base
+            if url_base
+            else "https://api.pushshift.io/reddit/search/submission/?"
+        )
+        self.params = params if params else {"size": 100, "sort": "desc"}
+        self.users = users
 
-    with open(file_name, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        for key, value in users.items():
-            writer.writerow([key, value])
+    def get_users(self, subreddit, file_name, after_date=None):
+        if after_date:
+            assert isinstance(after_date, dt.date), "'after' must be of type date"
+            if isinstance(after_date, dt.datetime):
+                after_date = after_date.date()
+            days = (dt.date.today() - after_date).days
+            after = f"&after={days}d"
+            self.params["after"] = f"{days}d"
+        else:
+            after = ""
+        self.params["subreddit"] = subreddit
+        users = self.users
+        ids = []
+        r = requests.get(
+            f"{self.url_base}subreddit={subreddit}&metadata=true&size=0{after}"
+        )
+        meta = json.loads(r.text)
+        for i in tqdm(range(ceil(meta["metadata"]["total_results"] / 100))):
+            r = requests.get(self.url_base, params=self.params)
+            if r.status_code == 429:
+                print("\ntoo many requests, retrying in 1s...")
+                print(self.params)
+                print(r.text)
+                time.sleep(1)
+            r = requests.get(self.url_base, params=self.params)
+            try:
+                j = json.loads(r.text)
+                for post in j["data"]:
+                    if flair := post["author_flair_text"]:
+                        users[post["author"]] = flair
+                    ids.append(post["id"])
+            except json.decoder.JSONDecodeError:
+                print(self.params)
+                print(r.text)
+            try:
+                self.params["before"] = j["data"][-1]["created_utc"]
+            except IndexError:
+                print(self.params)
+                print(r.text)
+                return
+            if not j["data"]:
+                break
+        with open(file_name, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            for key, value in users.items():
+                writer.writerow([key, value])
+        ids.reverse()
+        reddit = praw.Reddit()
+        x = 0
+        for id in tqdm(ids):
+            x += 1
+            post = reddit.submission(id)
+            comments = post.comments
+            comments.replace_more(limit=None)
+            for comment in comments.list():
+                if comment.author not in users:
+                    users[comment.author] = comment.author_flair_text
+            if x % 1000 == 0:
+                with open(file_name, "w", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    for key, value in users.items():
+                        writer.writerow([key, value])
+        with open(file_name, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            for key, value in users.items():
+                writer.writerow([key, value])
+        self.users = users
+        return users
 
-    ids.reverse()
-    reddit = praw.Reddit()
-    x = 0
-    for id in tqdm(ids):
-        x += 1
-        post = reddit.submission(id)
-        comments = post.comments
-        comments.replace_more(limit=None)
-        for comment in comments.list():
-            if comment.author not in users:
-                users[comment.author] = comment.author_flair_text
-        if x % 1000 == 0:
-            with open(file_name, 'w', newline='', encoding='utf-8') as file:
-                writer = csv.writer(file)
-                for key, value in users.items():
-                    writer.writerow([key, value])
+    def update(subreddit, file_name, last_post_date):
+        assert isinstance(
+            last_post_date, dt.date
+        ), "Date of last post must be Datetime or Date"
+        self.days = (dt.date.today() - after).days
+        return self.get_users(subreddit, file_name)
 
-    with open(file_name, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        for key, value in users.items():
-            writer.writerow([key, value])
 
 if __name__ == "__main__":
-    get_users(subreddit="mbti", file_name='Data\\mbti_.csv')
-    get_users(subreddit="Enneagram", file_name="Data\\Enneagram_.csv")
+    usergetter = UserGetter()
+    usergetter.get_users(subreddit="mbti", file_name="Data\\mbti_.csv")
+    usergetter.get_users(subreddit="Enneagram", file_name="Data\\Enneagram_.csv")
